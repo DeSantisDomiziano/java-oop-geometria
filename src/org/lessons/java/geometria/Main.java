a4ad45dcf61b6e6578ba4d2b70a4c7a22813136b
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-15 14:51:50 +0200

    add method in Rettangolo

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,5 +1,7 @@
 package org.lessons.java.geometria;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+	}
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -2,5 +2,29 @@ package org.lessons.java.geometria;
 
 public class Rettangolo {
 	
-
+	public boolean build;
+	public int altezza;
+	public int base;
+	
+	public Rettangolo() {
+		build = false;
+	}
+	
+	public Rettangolo(int altezza, int base) {
+		this();
+		
+		this.base = base;
+		this.altezza = altezza;
+	}
+	
+	public int getArea(int base, int altezza) {
+		
+		int area = this.base * this.altezza;
+		return area;
+	}
+	
+	public int getPerimetro() {
+		int perimetro = (2 * this.base) + (2 * this.altezza);
+		return perimetro;
+	}
 }
